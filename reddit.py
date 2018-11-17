1dd46f0be195930d8c3c1db138c2e39005c8835a
chinm1228 <33990451+chinm1228@users.noreply.github.com>
2018-11-17 15:36:26 -0800

    Update reddit.py

--- reddit.py
+++ reddit.py
@@ -10,6 +10,8 @@ from urllib import request
 #TODO: (3) Rename credentials_template.json to credentials.json
 #Tip: bash command for above step: cp credentials_template.json credentials.json
 
+dictionary = {'hello': 2, 'timing' : 7, 'weird': 5, 'the': 21, 'whatever': 15, 'yes': 8, 'a': 25}
+
 def create_reddit_instance(read_only = False):
     '''This function reads the crendentials from credentials.json and creates a Reddit instance.
         You will use this instance to retrieve further information. Before you complete this 
@@ -17,7 +19,7 @@ def create_reddit_instance(read_only = False):
         credentials_template.json and rename it to credentials.json
     '''
     credentials = None
-    #TODO: (4) Open 'credentials.json' and read the information into the credentials object initialized above. 
+    #TODO: (4) Open 'credential.json' and read the information into the credentials object initialized above. 
     with open('credentials.json', 'r') as creds:
         credentials = json.load(creds) 
     '''The read only parameter determines whether a read-only object,i.e, no login required, 
@@ -49,9 +51,31 @@ def ten_top_posts(reddit_instance, subreddit_name):
     #TODO: (7) Create a subreddit instance with subreddit_name and reddit_instance.
     subreddit = reddit_instance.subreddit(subreddit_name)
 
+
     #TODO: (8) Return the top posts in the subreddit by calling the .top() function on it.
     #TODO: (9) Set the limit parameter for the top function to 10, so that the top 10 posts are returned.
-    return subreddit.top(limit = 10)
+    return subreddit.top(limit = 2)
+
+def returnTitle_Description_Comments(post):
+    listComment = returnCommentArray(post)
+    contentArray = [post.title, post.selftext, listComment]
+    return contentArray
+
+def returnCommentArray(post):
+    listComment = []
+    for comment in post.comments:
+        listComment.append(comment.body)
+    return listComment
+
+def returnTop10Keywords(dictWords):
+    keywords = []
+    counter = 0
+    for key,value in sorted(dictWords.items(), key = lambda x: x[1], reverse = True):
+        if(counter <= 3):
+            if(len(key) > 4):
+                keywords.append(key)
+                counter += 1
+    return keywords
 
 if __name__ == '__main__':
     '''Experiment with your reddit function here. Examine the output on the console.'''
@@ -62,5 +86,11 @@ if __name__ == '__main__':
     #TODO: (12) Generate a list of top ten posts for your subreddit, using the praw Reddit object you created.
     posts = ten_top_posts(reddit_instance,subreddit_name)
     #TODO: (13) Iterate through the posts list returned by the function call above, and print its title attribute
-    for post in posts:
-        print(post.title)
+    #for post in posts:
+    #    contentArray = returnTitle_Description_Comments(post)
+    #    for content in contentArray:
+    #        print(content)
+    #        print('\n')
+
+    #print(returnTop10Keywords(dictionary))
+    
