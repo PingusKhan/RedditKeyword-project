8e95ff425518b1b052973aec43c1d016224c67dc
Sammy Truong <sammytruong@dhcp-v004-157.mobile.uci.edu>
2018-11-17 17:03:53 -0800

    changed search

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template
+from flask import request, redirect
 import json
 import reddit
-
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    #TODO: (14) Assign a Reddit subreddit name to use in this function.
-    subreddit_name = 'meme' #Assign this variable to your favorite subreddit.
-    #TODO: (15) Create an instance of the praw Reddit object. Refer to reddit.py
+def index(keyword=None):
+    if keyword== None:
+        subreddit_name = 'uci' 
+    else:
+        subreddit_name=keyword
     reddit_instance = reddit.create_reddit_instance(read_only =True)
-    #TODO: (16) Generate a list of top ten posts for your subreddit.
     top_ten_posts = reddit.ten_top_posts(reddit_instance, subreddit_name)
-    #TODO: (17) Pass the list you created above to the render_template function call below.
     return render_template('index.html',posts = top_ten_posts ,name = subreddit_name)
 
+@app.route('/handlesearch', methods=['POST'])
+def search_function():
+    return index(keyword=(request.form['name']))
+
 if __name__ == "__main__":
-    app.run(debug = True) #Set debug = False in a production environment
+    app.run(debug = True)
